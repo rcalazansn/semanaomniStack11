4543b6f6d2beb70fb907b28d7a90b9d18c97b1ae
rcalazansn <rcalazansn@gmail.com>
2020-03-29 19:19:08 -0300

    add tests

--- backend/src/app.js
+++ backend/src/app.js
@@ -10,4 +10,4 @@ app.use(express.json());
 app.use(routes);
 app.use(errors());
 
-app.listen(process.env.PORT || 3333);
+module.exports = app;
---
+++ backend/tests/unit/generateUniqueId.spec.js
@@ -0,0 +1,11 @@
+const generateUniqueId = require('../../src/utils/generateUniqueId');
+
+describe('Generate Unique ID', () => {
+    it('should generate an unique ID', () => {
+        //expect(2 + 2).toBe(4);
+        //expect(2 + 2).toBe(5);
+
+        const id = generateUniqueId();
+        expect(id).toHaveLength(8);
+    })
+});
